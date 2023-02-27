42569bd3550b5fe0664a8c0bd82d43f6b7f39102
gldkru <37909303+gldkru@users.noreply.github.com>
2023-02-27 22:34:08 +0400

    feat: generate grid from array, test set 'x'

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export const App = () => {
+  const [number, setNumber] = useState();
+
+  const handleClick = (index) => setNumber(index);
+
   return (
     <div className="grid">
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
+      {Array.from(Array(9)).map((_, index) => (
+        <div
+          className={`cell ${number === index ? 'x' : ''}`}
+          key={index}
+          onClick={() => handleClick(index)}
+        ></div>
+      ))}
     </div>
   );
 };
