4635bdc47b860d473bf1c60f9dcc95f8bd916123
gldkru <37909303+gldkru@users.noreply.github.com>
2023-02-27 22:18:34 +0400

    feat: initial grid

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+export const App = () => {
+  return (
+    <div className="grid">
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+      <div className="cell"></div>
+    </div>
+  );
+};
