6098b181b508fd9f22273b4115f13c8b0b8217c3
gldkru <37909303+gldkru@users.noreply.github.com>
2023-03-06 22:03:48 +0400

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,34 @@ import React, { useState } from 'react';
 import './style.css';
 
 export const App = () => {
-  const [number, setNumber] = useState();
+  const [xState, setXState] = useState([]);
+  const [oState, setOState] = useState([]);
+  const [xTurn, setXTurn] = useState(true);
+  // const [number, setNumber] = useState();
 
-  const handleClick = (index) => setNumber(index);
+  const handleClick = (index) => {
+    if (xTurn) {
+      setXState([...xState, index]);
+    } else {
+      setOState([...oState, index]);
+    }
+
+    setXTurn(!xTurn);
+  };
+
+  const compare = (index) => {
+    if (xState.includes(index)) {
+      return 'x';
+    } else if (oState.includes(index)) {
+      return 'o';
+    }
+  };
 
   return (
     <div className="grid">
       {Array.from(Array(9)).map((_, index) => (
         <div
-          className={`cell ${number === index ? 'x' : ''}`}
+          className={`cell ${compare(index)}`}
           key={index}
           onClick={() => handleClick(index)}
         ></div>
